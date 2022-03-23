e6451261c41f62062299801a7f8111ae7ca38b38
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-03-23 15:32:56 -0300

    Initial commit

---
+++ Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionObject : MonoBehaviour
+{
+    public enum  InteractableType
+    {
+        nothing,
+        info,
+        pickup,
+        dialoge
+    }
+    [Header("Type of Interactable")]
+    public InteractableType interType;
+
+    [Header("Simple info Message")]
+    public string infoMessage;
+    private Text infoText;
+
+    public void Start()
+    {
+        infoText = GameObject.Find("InfoText").GetComponent<Text>();
+    }
+
+    public void Nothing()
+    {
+
+    }
+    public void Info()
+    {
+        infoText.text = infoMessage;
+    }
+}
